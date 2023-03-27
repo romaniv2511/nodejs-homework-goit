abb02f42ebe6d1ccbcd2685e5ccf1a9be9ebf74b
Alexandr Romaniv <alexandr.romaniv@mtrading.com>
2023-03-27 23:27:40 +0300

    add hw mongoDB

--- helpers/ctrlWrapper.js
+++ helpers/ctrlWrapper.js
@@ -2,10 +2,11 @@ const ctrlWrapper = (ctrl) => {
   const func = async (req, res, next) => {
     try {
       await ctrl(req, res, next);
-    } catch (error) {
-      next(error);
+    } catch (e) {
+      next(e);
     }
   };
   return func;
 };
+
 module.exports = ctrlWrapper;
